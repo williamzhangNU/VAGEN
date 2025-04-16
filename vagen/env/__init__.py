faf9032037d6e3157d30e9f6e4d774cb9b0b83ce
YaningGao <20082585d@gmail.com>
2025-04-15 23:15:43 -0500

    comment

--- vagen/env/__init__.py
+++ vagen/env/__init__.py
@@ -1,8 +1,8 @@
 from .sokoban import SokobanEnv,SokobanConfig
 from .frozenlake import FrozenLakeEnv,FrozenLakeConfig, FrozenLakeService
-from .navigation import NavigationEnv, NavigationConfig
-from .svgdino import SVGDINOEnv, SVGDINOConfig
-from .svg import SVGEnv, SVGConfig, SVGService
+# from .navigation import NavigationEnv, NavigationConfig
+# from .svgdino import SVGDINOEnv, SVGDINOConfig
+# from .svg import SVGEnv, SVGConfig, SVGService
 
 REGISTERED_ENV = {
     "sokoban": {
@@ -14,17 +14,17 @@ REGISTERED_ENV = {
         "config_cls": FrozenLakeConfig,
         "service_cls": FrozenLakeService
     },
-    "navigation": {
-        "env_cls": NavigationEnv,
-        "config_cls": NavigationConfig
-    },
-    "svg": {
-        "env_cls": SVGEnv,
-        "config_cls": SVGConfig,
-        "service_cls": SVGService
-    },
-    "svgdino": {
-        "env_cls": SVGDINOEnv,
-        "config_cls": SVGDINOConfig,
-    }
+    # "navigation": {
+    #     "env_cls": NavigationEnv,
+    #     "config_cls": NavigationConfig
+    # },
+    # "svg": {
+    #     "env_cls": SVGEnv,
+    #     "config_cls": SVGConfig,
+    #     "service_cls": SVGService
+    # },
+    # "svgdino": {
+    #     "env_cls": SVGDINOEnv,
+    #     "config_cls": SVGDINOConfig,
+    # }
 }
