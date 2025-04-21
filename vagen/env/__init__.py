6ef9a387b7ee9ea72dc7835d65648b131885d0e6
jameskrw <kangruiwang.cs@gmail.com>
2025-04-21 14:13:01 -0500

    minor

--- vagen/env/__init__.py
+++ vagen/env/__init__.py
@@ -2,7 +2,7 @@ from .sokoban import SokobanEnv,SokobanEnvConfig
 from .frozenlake import FrozenLakeEnv,FrozenLakeEnvConfig, FrozenLakeService
 from .navigation import NavigationEnv, NavigationEnvConfig, NavigationServiceConfig, NavigationService
 from .svg import SVGEnv, SvgEnvConfig, SVGService, SVGServiceConfig
-from .primitive_skill import PrimitiveSkillEnv, PrimitiveSkillEnvConfig, PrimitiveSkillService, PrimitiveSkillConfig
+from .primitive_skill import PrimitiveSkillEnv, PrimitiveSkillEnvConfig, PrimitiveSkillService, PrimitiveSkillServiceConfig
 
 REGISTERED_ENV = {
     "sokoban": {
@@ -30,6 +30,6 @@ REGISTERED_ENV = {
     #     "env_cls": PrimitiveSkillEnv,
     #     "config_cls": PrimitiveSkillEnvConfig,
     #     "service_cls": PrimitiveSkillService,
-    #     "service_config_cls": PrimitiveSkillConfig
+    #     "service_config_cls": PrimitiveSkillServiceConfig
     # }
 }
