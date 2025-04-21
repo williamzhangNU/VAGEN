7cbb61f4c5e99d7b3a10dd047163c46b804b9f17
jameskrw <kangruiwang.cs@gmail.com>
2025-04-21 14:12:42 -0500

    minor

--- vagen/env/primitive_skill/__init__.py
+++ vagen/env/primitive_skill/__init__.py
@@ -1,4 +1,4 @@
 from .env import PrimitiveSkillEnv
 from .env_config import PrimitiveSkillEnvConfig
 from .service import PrimitiveSkillService
-from .service_config import PrimitiveSkillConfig
+from .service_config import PrimitiveSkillServiceConfig
