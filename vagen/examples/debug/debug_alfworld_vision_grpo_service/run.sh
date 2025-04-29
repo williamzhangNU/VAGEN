6b8f8281250caa81ec4dbe8ff28437545c80c197
root <20082585d@gmail.com>
2025-04-29 08:42:50 +0000

    alfworld update

--- vagen/examples/debug/debug_alfworld_vision_grpo_service/run.sh
+++ vagen/examples/debug/debug_alfworld_vision_grpo_service/run.sh
@@ -59,7 +59,7 @@ python3 -m vagen.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='vagen_debug' \
-    trainer.experiment_name='grpo_mask_loss_alfworld_text_debug' \
+    trainer.experiment_name='grpo_mask_loss_alfworld_vision_debug' \
     trainer.n_gpus_per_node=1 \
     trainer.nnodes=1 \
     trainer.save_freq=100 \
