9cbea459e125bc3609c04eba87c2a7d2eda4b369
root <20082585d@gmail.com>
2025-04-30 21:56:01 +0000

    update

--- vagen/env/__init__.py
+++ vagen/env/__init__.py
@@ -1,9 +1,9 @@
 from .sokoban import SokobanEnv,SokobanEnvConfig
 from .frozenlake import FrozenLakeEnv,FrozenLakeEnvConfig, FrozenLakeService
-from .navigation import NavigationEnv, NavigationEnvConfig, NavigationServiceConfig, NavigationService
+# from .navigation import NavigationEnv, NavigationEnvConfig, NavigationServiceConfig, NavigationService
 # from .svg import SVGEnv, SvgEnvConfig, SVGService, SVGServiceConfig
 # from .primitive_skill import PrimitiveSkillEnv, PrimitiveSkillEnvConfig, PrimitiveSkillService, PrimitiveSkillServiceConfig
-# from .alfworld import ALFWorldEnv, ALFWorldEnvConfig, ALFWorldService, ALFWorldServiceConfig
+from .alfworld import ALFWorldEnv, ALFWorldEnvConfig, ALFWorldService, ALFWorldServiceConfig
 REGISTERED_ENV = {
     "sokoban": {
         "env_cls": SokobanEnv,
@@ -14,12 +14,12 @@ REGISTERED_ENV = {
         "config_cls": FrozenLakeEnvConfig,
         "service_cls": FrozenLakeService
     },
-    "navigation": {
-        "env_cls": NavigationEnv,
-        "config_cls": NavigationEnvConfig,
-        "service_cls": NavigationService,
-        "service_config_cls": NavigationServiceConfig
-    },
+    # "navigation": {
+    #     "env_cls": NavigationEnv,
+    #     "config_cls": NavigationEnvConfig,
+    #     "service_cls": NavigationService,
+    #     "service_config_cls": NavigationServiceConfig
+    # },
     # "svg": {
     #     "env_cls": SVGEnv,
     #     "config_cls": SvgEnvConfig,
@@ -32,10 +32,10 @@ REGISTERED_ENV = {
     #     "service_cls": PrimitiveSkillService,
     #     "service_config_cls": PrimitiveSkillServiceConfig
     # },
-    # "alfworld": {
-    #     "env_cls": ALFWorldEnv,
-    #     "config_cls": ALFWorldEnvConfig,
-    #     "service_cls": ALFWorldService,
-    #     "service_config_cls": ALFWorldServiceConfig
-    # },
+    "alfworld": {
+        "env_cls": ALFWorldEnv,
+        "config_cls": ALFWorldEnvConfig,
+        "service_cls": ALFWorldService,
+        "service_config_cls": ALFWorldServiceConfig
+    },
 }
