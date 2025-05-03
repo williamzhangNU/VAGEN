bdbada26496ad07dc07818ce9a684616bb0a47e0
YaningGao <20082585d@gmail.com>
2025-05-03 04:37:19 -0500

    add openai api

--- vagen/mllm_agent/model_interface/__init__.py
+++ vagen/mllm_agent/model_interface/__init__.py
@@ -1,8 +1,13 @@
 from .vllm import VLLMModelInterface, VLLMModelConfig
+from .openai import OpenAIModelInterface, OpenAIModelConfig
 
 REGISTERED_MODEL = {
     "vllm": {
         "model_cls": VLLMModelInterface,
         "config_cls": VLLMModelConfig,
     },
+    "openai": {
+        "model_cls": OpenAIModelInterface,
+        "config_cls": OpenAIModelConfig
+    }
 }
