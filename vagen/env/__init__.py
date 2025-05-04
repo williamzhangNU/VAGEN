b35ee7443f56f43edbe9a05c120be742b9732f95
YaningGao <20082585d@gmail.com>
2025-05-03 23:45:58 -0500

    update sokoban

--- vagen/env/__init__.py
+++ vagen/env/__init__.py
@@ -1,4 +1,4 @@
-from .sokoban import SokobanEnv,SokobanEnvConfig
+from .sokoban import SokobanEnv, SokobanEnvConfig, SokobanService, SokobanServiceConfig
 from .frozenlake import FrozenLakeEnv,FrozenLakeEnvConfig, FrozenLakeService
 # from .navigation import NavigationEnv, NavigationEnvConfig, NavigationServiceConfig, NavigationService
 # from .svg import SVGEnv, SvgEnvConfig, SVGService, SVGServiceConfig
@@ -9,6 +9,8 @@ REGISTERED_ENV = {
     "sokoban": {
         "env_cls": SokobanEnv,
         "config_cls": SokobanEnvConfig,
+        "service_cls": SokobanService,
+        "service_config_cls": SokobanServiceConfig
     },
     "frozenlake": {
         "env_cls": FrozenLakeEnv,
