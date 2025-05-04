057c26f9d6a4a86497fac96b341538a7cbf4e659
jameskrw <kangruiwang.cs@gmail.com>
2025-05-04 14:05:04 +0000

    updated scripts

--- scripts/exps/format/navigation/grounding/run.sh
+++ scripts/exps/format/navigation/grounding/run.sh
@@ -29,8 +29,8 @@ python3 -m vagen.trainer.main_ppo \
     data.train_batch_size=128 \
     data.val_batch_size=8 \
     data.max_prompt_length=1024 \
-    data.max_response_length=190 \
-    data.max_trajectory_length=4080 \
+    data.max_response_length=256 \
+    data.max_trajectory_length=5000 \
     data.image_key=images \
     data.truncation=error \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-VL-3B-Instruct \
