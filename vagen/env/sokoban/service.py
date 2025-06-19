a484fb534faeb22620131c54de8b54e6ebc182bf
jameskrw <kangruiwang.cs@gmail.com>
2025-06-19 14:35:25 +0000

    updated sokoban for v4 reward

--- vagen/env/sokoban/service.py
+++ vagen/env/sokoban/service.py
@@ -1,19 +1,24 @@
 from typing import Dict, List, Tuple, Optional, Any, Union
 from vagen.env.base.base_service import BaseService
 from vagen.env.base.base_service_config import BaseServiceConfig
-from vagen.env.utils.state_reward_text_utils import service_state_reward_wrapper_v2 as service_state_reward_wrapper
+# from vagen.env.utils.state_reward_text_utils import service_state_reward_wrapper_v2 as service_state_reward_wrapper
 from vagen.server.serial import serialize_observation
 
 from .env import SokobanEnv
 from .env_config import SokobanEnvConfig
+from vagen.env.utils.state_reward_text_utils import service_state_reward_wrapper_v3 as service_state_reward_wrapper
 from .prompt import visual_reasoning_reward_prompt
-from vagen.env.utils.state_matching import calculate_visual_reasoning_reward_bipartite
+from vagen.env.utils.state_matching import calculate_visual_reasoning_reward_bipartite,calculate_f1_with_max_matching
+from vagen.env.utils.top_string_tracker import TopKStringTracker
 class SokobanService(BaseService):
     
     def __init__(self, config: BaseServiceConfig):
         self.environments = {}
         self.env_configs = {}
         self.config = config
+        if self.config.use_state_reward:
+            self.top_strings_tracker_grounding = TopKStringTracker(self.config.top_strings_m)
+            self.top_strings_tracker_worldmodeling = TopKStringTracker(self.config.top_strings_m)
     
     def create_environments_batch(self, ids2configs: Dict[Any, Any]) -> None:
         for env_id, config in ids2configs.items():
@@ -79,18 +84,40 @@ class SokobanService(BaseService):
     def gen_visual_reasoning_prompt(self, content,**kwargs) -> str:
         return visual_reasoning_reward_prompt.format(prediction=content)
     
-    def calculate_visual_reasoning_reward(self, response,state) -> float:
+    def calculate_visual_reasoning_reward(self, response,state,content,r_type,**kwargs) -> float:
         """
         Calculate the visual reasoning reward based on the response and state.
-        
+        e.g. [{"object_id": "target", "vertical_relation":above,"horizontal_relation":left}, 
+            {"object_id": "hole", "vertical_relation":above,"horizontal_relation":left}]
         Args:
-            response: The response from the LLM.
+            response: The output of the llm judge (structured state).
             state: The current state of the environment.
+            content: The input to the llm judge (natural lanagugae state).
         
         Returns:
             A float representing the calculated reward.
         """
-        object_weights={"target": 0.5,"box": 0.5}
+        # object_weights={"target": 0.7,"hole": 0.3}
+        # return calculate_visual_reasoning_reward_bipartite(response, state,object_weights)
+        target_result = calculate_f1_with_max_matching(
+            [item for item in state if item['object_id'] == 'target'],
+            [item for item in response if item['object_id'] == 'target'],
+            match_func=lambda x, y: x['vertical_relation'] == y['vertical_relation'] and x['horizontal_relation'] == y['horizontal_relation']
+        )
+        # check hole reward
+        box_result =calculate_f1_with_max_matching(
+            [item for item in state if item['object_id'] == 'box'],
+            [item for item in response if item['object_id'] == 'box'],
+            match_func=lambda x, y: x['vertical_relation'] == y['vertical_relation'] and x['horizontal_relation'] == y['horizontal_relation']
+        )
+        target_reward = target_result['f1']
+        box_reward = box_result['f1']
+        if r_type=="grounding":
+            top_k_strings = self.top_strings_tracker_grounding.get_top_k(self.config.top_strings_k)
+        if r_type=="worldmodeling":
+            top_k_strings = self.top_strings_tracker_worldmodeling.get_top_k(self.config.top_strings_k)
         
-        return calculate_visual_reasoning_reward_bipartite(response, state,object_weights)
+        if content in top_k_strings and target_reward+box_reward<0.7:
+            return -0.1
+        return target_reward*0.5 + box_reward*0.5
         
