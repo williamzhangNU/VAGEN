1b85ce24040aea720e3740d0155176e409f0c525
williamzhangNU <pingyuezhang2029@u.northwestern.edu>
2025-12-16 14:43:17 -0600

    update tasks, message builder

--- vagen/env/spatial/message_list_builder.py
+++ vagen/env/spatial/message_list_builder.py
@@ -139,6 +139,12 @@ def build_evaluation_from_combo(
             while task.eval_data.id in existing_id_for_task and retry:
                 q_text = task.generate_question()
                 retry -= 1
+            if task.eval_data.id in existing_id_for_task:
+                force_retry = 5
+                while task.eval_data.id in existing_id_for_task and force_retry:
+                    setattr(task, "_force_shuffle_final_obs", True)
+                    q_text = task.generate_question()
+                    force_retry -= 1
             assert task.eval_data.id not in existing_id_for_task, f"Failed to generate unique question for {task_short} in {combo_dir}"
             existing_id_for_task.append(task.eval_data.id)
             assert base_msgs[-1]["role"] == "user"
